01156b3357317be04fe06066922ac350e521e58b
Daniil-Solo <daniil.solo1723@gmail.com>
2024-07-07 12:45:19 +0300

    add base non-protected page - courses

---
+++ src/page/courses/index.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from "../../layouts/default-layout";
+
+
+export default function CoursesPage(){
+    return (
+        <>
+            <DefaultLayout>
+                <div>
+                    Курсы
+                </div>
+            </DefaultLayout>
+        </>
+    )
+}
