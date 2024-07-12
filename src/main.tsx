43e4307aade3f414fc9bd7074e8cba3448dc584a
Daniil-Solo <daniil.solo1723@gmail.com>
2024-07-12 11:55:21 +0300

    add rq provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import { createTheme, MantineProvider } from '@mantine/core';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import router from "./router";
 import './index.css'
 import './reset.css'
@@ -27,6 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <MantineProvider theme={theme} withGlobalClasses>
             <QueryClientProvider client={queryClient}>
                 <RouterProvider router={router} />
+                <ReactQueryDevtools initialIsOpen />
             </QueryClientProvider>
         </MantineProvider>
     </React.StrictMode>
