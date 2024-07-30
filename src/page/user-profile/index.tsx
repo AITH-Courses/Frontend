183896513be41df49c9407a09e38412914d1c39b
Daniil-Solo <daniil.solo1723@gmail.com>
2024-07-30 11:08:32 +0300

    correct profile page with correct header view

--- src/page/user-profile/index.tsx
+++ src/page/user-profile/index.tsx
@@ -1,18 +1,23 @@
 import React, {useEffect, useState} from "react";
-import {Stack, Skeleton, Button, Fieldset, TextInput, SimpleGrid, Text, Title, Space} from "@mantine/core";
+import {Stack, Button, Fieldset, TextInput, SimpleGrid, Text, Title, Space, Group, PasswordInput} from "@mantine/core";
 import DefaultLayout from "../../layouts/default-layout";
 import {useMe} from "../../hooks/auth";
 import {IUser} from "../../types/auth.ts";
 import axios, {AxiosError} from "axios";
 import {useNavigate} from "react-router-dom";
 import {IFailedOperation} from "../../types/base.ts";
+import AdminLayout from "../../layouts/admin-layout";
 
 
 
 export default function UserProfilePage(){
     const navigate = useNavigate();
-    const {data, isSuccess, isError, error} = useMe();
-    const [user, setUser] = useState<IUser | undefined>(undefined)
+    const {data, isSuccess, isError, error, isLoading} = useMe();
+    const [user, setUser] = useState<IUser | null>(null)
+
+    useEffect(() => {
+        setUser(data as IUser);
+    }, [data]);
 
     if (isError && axios.isAxiosError(error)){
         const e = error as AxiosError<IFailedOperation, never>;
@@ -24,82 +29,96 @@ export default function UserProfilePage(){
         }
         navigate("/login");
     }
-    if (isSuccess && user && user.role === "admin"){
-        navigate("/admin/courses");
+
+    if (isLoading || user == null){
+        return (
+            <DefaultLayout>
+                <Title order={2}>
+                    Загрузка
+                </Title>
+            </DefaultLayout>
+        )
     }
 
-    useEffect(() => {
-        setUser(data as IUser);
-    }, [data]);
+    const changePersonalInfoFieldset = (
+        isSuccess && user
+        ? (
+            <Fieldset legend={<Text fw={600} size="md">Персональная информация</Text>}>
+                <Stack gap="xs">
+                    <TextInput
+                        label="Имя"
+                        placeholder="Иван..."
+                        value={user.firstname}
+                        onChange={e => user && setUser({...user, firstname: e.target.value})}
+                    />
+                    <TextInput
+                        label="Фамилия"
+                        placeholder="Иванов..."
+                        value={user.lastname}
+                        onChange={e => user && setUser({...user, lastname: e.target.value})}
+                    />
+                    <Group justify="flex-end">
+                        <Button size="xs" variant="filled" color="dark">
+                            Сохранить изменения
+                        </Button>
+                    </Group>
+                </Stack>
+            </Fieldset>
+        ): null
+    )
+
+    const changePasswordFieldset = (
+        isSuccess && user
+            ? (
+                <Fieldset legend={<Text fw={600} size="md">Изменение пароля</Text>} >
+                    <Stack gap="xs">
+                        <PasswordInput
+                            size={"sm"}
+                            label="Старый пароль"
+                        />
+                        <PasswordInput
+                            size={"sm"}
+                            label="Новый пароль"
+                        />
+                        <PasswordInput
+                            size={"sm"}
+                            label="Повтор пароля"
+                        />
+                        <Group justify="flex-end">
+                            <Button size="xs" variant="filled" color="dark">
+                                Сохранить изменения
+                            </Button>
+                        </Group>
+                    </Stack>
+                </Fieldset>
+            ): null
+    )
+
+    if (isSuccess && user && user.role === "admin"){
+        return (
+            <AdminLayout>
+                <Title order={2}>
+                    {"Администратор " + user.firstname}
+                </Title>
+                <Space h="xs" />
+                <SimpleGrid cols={{ xs: 1, md: 2, lg: 3 }} spacing="xs" verticalSpacing="xs">
+                    {changePersonalInfoFieldset}
+                    {changePasswordFieldset}
+                </SimpleGrid>
+            </AdminLayout>
+        )
+    }
 
     return (
         <>
             <DefaultLayout>
-                {
-                    isSuccess && user
-                        ? (
-                            <Title order={1}>
-                                {
-                                    user.role === "admin"
-                                    ? "Администратор " + user.firstname
-                                    : "Талант " + user.firstname
-                                }
-                            </Title>
-                        )
-                        : (<Skeleton height={44} width={"100%"} />)
-                }
+                <Title order={1}>
+                    {"Талант " + user.firstname}
+                </Title>
                 <Space h="xs" />
                 <SimpleGrid cols={{ xs: 1, md: 2, lg: 3 }} spacing="xs" verticalSpacing="xs">
-                    <Fieldset legend={<Text fw={600} size="md">Персональная информация</Text>}>
-                        <Stack gap="xs">
-                            {
-                                isSuccess && user
-                                    ? (
-                                        <TextInput
-                                            label="Имя"
-                                            placeholder="Иван..."
-                                            value={user.firstname}
-                                            onChange={e => user && setUser({...user, firstname: e.target.value})}
-                                        />
-                                    )
-                                    : (<Skeleton height={60} width={"100%"} />)
-                            }
-                            {
-                                isSuccess && user
-                                    ? (
-                                        <TextInput
-                                            label="Фамилия"
-                                            placeholder="Фамилия..."
-                                            value={user.lastname}
-                                            onChange={e => user && setUser({...user, lastname: e.target.value})}
-                                        />
-                                    )
-                                    : (<Skeleton height={60} width={"100%"} />)
-                            }
-                            <Button size="xs" variant="filled" color="dark">
-                                Сохранить изменения
-                            </Button>
-                        </Stack>
-                    </Fieldset>
-                    {/*<Fieldset legend={(<span style={{fontSize: "20px", fontWeight: "bold"}}>Изменение пароля</span>)}>*/}
-                    {/*    <Stack gap="xs">*/}
-                    {/*        <PasswordInput*/}
-                    {/*            size={"sm"}*/}
-                    {/*            label="Старый пароль"*/}
-                    {/*        />*/}
-                    {/*        <PasswordInput*/}
-                    {/*            size={"sm"}*/}
-                    {/*            label="Новый пароль"*/}
-                    {/*        />*/}
-                    {/*        <PasswordInput*/}
-                    {/*            size={"sm"}*/}
-                    {/*            label="Повтор пароля"*/}
-                    {/*        />*/}
-                    {/*        <Button size="xs" variant="filled" color="dark">*/}
-                    {/*            Сохранить изменения*/}
-                    {/*        </Button>*/}
-                    {/*    </Stack>*/}
-                    {/*</Fieldset>*/}
+                    {changePersonalInfoFieldset}
+                    {changePasswordFieldset}
                 </SimpleGrid>
             </DefaultLayout>
         </>
