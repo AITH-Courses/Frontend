d508a2c62aa67980dd2d585682a640c96261b5a4
Daniil-Solo <daniil.solo1723@gmail.com>
2024-08-17 15:41:16 +0300

    correct 404 page

--- src/page/404/index.tsx
+++ src/page/404/index.tsx
@@ -9,11 +9,9 @@ export const Page404 = () => {
         <Container className="layout-404">
             <Stack>
                 <p className="layout-404__status">404</p>
-                <Title ta="center">Что-то пошло не так...</Title>
+                <Title ta="center">Кажется, тут ничего нет ...</Title>
                 <Text c="dimmed" size="lg" ta="center" >
                     Страница, которую вы пытаетесь открыть, не существует.
-                    Возможно, вы неправильно ввели адрес или страница была перемещена на другой URL.
-                    Если вы считаете, что это ошибка, обратитесь в службу поддержки.
                 </Text>
                 <Group justify="center">
                     <Button variant="filled" size="md" color="dark" onClick={() => navigate("/")}>
