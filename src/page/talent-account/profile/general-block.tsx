72a18f74319f73c6044a998fbd026101e55d7357
Daniil-Solo <daniil.solo1723@gmail.com>
2024-08-30 20:14:04 +0300

    add edit mode to change profile

---
+++ src/page/talent-account/profile/general-block.tsx
@@ -0,0 +1,134 @@
+import {ActionIcon, Avatar, Divider, Group, Skeleton, Stack, Text, Title} from "@mantine/core";
+import {IconPencil} from "@tabler/icons-react";
+import React from "react";
+import {ITalentProfile} from "../../../types/talent-profile.ts";
+
+interface GeneralBlockProps {
+    profile: ITalentProfile | null,
+    isLoading: boolean,
+    isSuccess: boolean,
+    startEditMode: () => void
+}
+
+const GeneralBlock: React.FC<GeneralBlockProps> = (props) => {
+    const {profile, isLoading, isSuccess, startEditMode} = props;
+    return (
+        <div className="block">
+            <ActionIcon variant="transparent" size="xl" aria-label="Edit" pos="absolute" top="24px" right="24px" onClick={startEditMode} title="Редактировать профиль">
+                <IconPencil color="black" size="2rem"  stroke={1} />
+            </ActionIcon>
+            <Group>
+                {
+                    isLoading
+                        ? <Skeleton height={100} circle/>
+                        : isSuccess && profile && (
+                        <Avatar
+                            src={profile.image_url}
+                            alt={profile.firstname + " " + profile.lastname}
+                            color="initials"
+                            name={profile.firstname + " " + profile.lastname}
+                            radius={100}
+                            size={100}
+                            mih={100}
+                        />
+                    )
+                }
+                <Stack justify="flex-start" align="flex-start" gap="xs">
+                    {
+                        isLoading
+                            ? <Skeleton height={30} width={300}/>
+                            : isSuccess && profile && (
+                            <Title order={3} ta="left">
+                                {profile.firstname + " " + profile.lastname}
+                            </Title>
+                        )
+                    }
+                    {
+                        isLoading
+                            ? <Skeleton height={30} width={100}/>
+                            : isSuccess && profile && (
+                            <Text c="dimmed" size="lg" ta="left" >
+                                Талант
+                            </Text>
+                        )
+                    }
+                </Stack>
+            </Group>
+            <Divider my={"md"}/>
+            <Group gap="xs" hiddenFrom={"xs"}>
+                {
+                    isLoading
+                        ? <Skeleton height={30} width={"25%"}/>
+                        : isSuccess && profile && profile.location !== "" && (
+                        <Text c="black" size="lg" ta="left" >
+                            {profile.location},
+                        </Text>
+                    )
+                }
+                {
+                    isLoading
+                        ? <Skeleton height={30} width={"25%"}/>
+                        : isSuccess && profile && profile.company !== "" && (
+                        <Text c="black" size="lg" ta="left" >
+                            {profile.company},
+                        </Text>
+                    )
+                }
+                {
+                    isLoading
+                        ? <Skeleton height={30} width={"25%"}/>
+                        : isSuccess && profile && profile.position !== "" && (
+                        <Text c="black" size="lg" ta="left" >
+                            {profile.position}
+                        </Text>
+                    )
+                }
+            </Group>
+            <Stack gap="xs" visibleFrom={"xs"}>
+                {
+                    isLoading
+                        ? <Skeleton height={30} width={"100%"}/>
+                        : isSuccess && profile && profile.location !== "" && (
+                        <Group gap="xs">
+                            <Text fw={"bold"} c="black" size="lg" ta="left" w={120} >
+                                Локация
+                            </Text>
+                            <Text c="black" size="lg" ta="left" >
+                                {profile.location}
+                            </Text>
+                        </Group>
+                    )
+                }
+                {
+                    isLoading
+                        ? <Skeleton height={30} width={"100%"}/>
+                        : isSuccess && profile && profile.company !== "" && (
+                        <Group gap="xs">
+                            <Text fw={"bold"} c="black" size="lg" ta="left" w={120} >
+                                Компания
+                            </Text>
+                            <Text c="black" size="lg" ta="left" >
+                                {profile.company}
+                            </Text>
+                        </Group>
+                    )
+                }
+                {
+                    isLoading
+                        ? <Skeleton height={30} width={"100%"}/>
+                        : isSuccess && profile && profile.position !== "" && (
+                        <Group gap="xs">
+                            <Text fw={"bold"} c="black" size="lg" ta="left" w={120} >
+                                Позиция
+                            </Text>
+                            <Text c="black" size="lg" ta="left" >
+                                {profile.position}
+                            </Text>
+                        </Group>
+                    )
+                }
+            </Stack>
+        </div>
+    )
+}
+export default GeneralBlock;
--- src/page/talent-account/profile/index.tsx
+++ src/page/talent-account/profile/index.tsx
@@ -2,172 +2,118 @@ import TalentProfileLayout from "../../../layouts/talent-profile-layout";
 import {
     ActionIcon,
     Anchor,
-    Avatar,
-    Badge,
     Divider,
     Grid,
-    Group,
     Skeleton,
     Stack,
-    Text,
     Title
 } from "@mantine/core";
 import "./index.css";
-import {useMe} from "../../../hooks/auth";
 import React, {useEffect, useState} from "react";
-import {IUser} from "../../../types/auth.ts";
 import {useNavigate} from "react-router-dom";
 import {IconPencil} from "@tabler/icons-react";
+import {useProfile, useUpdateProfileGeneral} from "../../../hooks/talent-profile/profile.ts";
+import {ITalentProfile} from "../../../types/talent-profile.ts";
+import GeneralBlock from "./general-block.tsx";
+import GeneralEditBlock from "./general-edit-block.tsx";
 
 const ProfilePage = () => {
     const navigate = useNavigate()
-    const {data, isSuccess, isError, isLoading} = useMe();
-    const [user, setUser] = useState<IUser | null>(null)
+    const {data, isSuccess, isError, isLoading} = useProfile();
+    const {mutateAsync: updateProfileGeneral} = useUpdateProfileGeneral()
+    const [profile, setProfile] = useState<ITalentProfile | null>(null)
+    const [isEditGeneral, setIsEditeGeneral] = useState(false);
 
     useEffect(() => {
-        setUser(data as IUser);
+        setProfile(data as ITalentProfile);
     }, [data]);
 
     if (isError){
         navigate("/login")
     }
 
+    const saveProfileGeneral = (profile: ITalentProfile) => {
+        setIsEditeGeneral(false);
+        updateProfileGeneral(profile)
+    }
+
     return (
         <TalentProfileLayout>
             <Grid gutter={28} h={"100%"}>
                 <Grid.Col span={{ base: 12, md: 8 }}>
-                    <div className="block">
-                        <ActionIcon variant="transparent" size="xl" aria-label="Settings" pos="absolute" top="24px" right="24px">
-                            <IconPencil color="black" size="2rem"  stroke={1} />
-                        </ActionIcon>
-                        <Group>
-                            {
-                                isLoading
-                                ? <Skeleton height={100} circle/>
-                                : isSuccess && user && (
-                                    <Avatar
-                                        src=""
-                                        alt={user.firstname + " " + user.lastname}
-                                        color="initials"
-                                        name={user.firstname + " " + user.lastname}
-                                        radius={100}
-                                        size={100}
-                                    />
-                                )
-                            }
-                            <Stack justify="flex-start" align="flex-start" gap="xs">
-                                {
-                                    isLoading
-                                        ? <Skeleton height={30} width={300}/>
-                                        : isSuccess && user && (
-                                            <Title order={3} ta="left">
-                                                {user.firstname + " " + user.lastname}
-                                            </Title>
-                                        )
-                                }
-                                {
-                                    isLoading
-                                        ? <Skeleton height={30} width={100}/>
-                                        : isSuccess && user && (
-                                        <Text c="dimmed" size="lg" ta="left" >
-                                            Талант
-                                        </Text>
-                                    )
-                                }
-                            </Stack>
-                        </Group>
-                        <Divider my={"md"}/>
-                        <Group gap="xs" hiddenFrom={"xs"}>
-                            <Text c="black" size="lg" ta="left" >
-                                Россия/Москва,
-                            </Text>
-                            <Text c="black" size="lg" ta="left" >
-                                Яндекс,
-                            </Text>
-                            <Text c="black" size="lg" ta="left" >
-                                NLP Researcher
-                            </Text>
-                            <Anchor w={"100%"} href="https://t.me/user325235234653" target="_blank" underline="hover">
-                                @user325235234653
-                            </Anchor>
-                            {/*<Button*/}
-                            {/*    variant="outline"*/}
-                            {/*    rightSection={<IconBrandTelegram/>}*/}
-                            {/*    onClick={() => window.open("https://t.me/user325235234653", "blank")}*/}
-                            {/*>*/}
-                            {/*    Написать в telegram*/}
-                            {/*</Button>*/}
-                        </Group>
-                        <Stack gap="xs" visibleFrom={"xs"}>
-                            <Group gap="xs">
-                                <Text fw={"bold"} c="black" size="lg" ta="left" w={120} >
-                                    Локация
-                                </Text>
-                                <Text c="black" size="lg" ta="left" >
-                                    Россия/Москва
-                                </Text>
-                            </Group>
-                            <Group gap="xs">
-                                <Text fw={"bold"}  c="black" size="lg" ta="left" w={120} >
-                                    Компания
-                                </Text>
-                                <Text c="black" size="lg" ta="left" >
-                                    Яндекс
-                                </Text>
-                            </Group>
-                            <Group gap="xs">
-                                <Text fw={"bold"}  c="black" size="lg" ta="left" w={120}>
-                                    Позиция
-                                </Text>
-                                <Text c="black" size="lg" ta="left" >
-                                    NLP Researcher
-                                </Text>
-                            </Group>
-                            <Group gap="xs">
-                                <Text fw={"bold"}  c="black" size="lg" ta="left" w={120}>
-                                    Контакт
-                                </Text>
-                                <Anchor href="https://t.me/user325235234653" target="_blank" underline="hover">
-                                    @user325235234653
-                                </Anchor>
-                            </Group>
-                        </Stack>
-                    </div>
+                    {
+                        isEditGeneral
+                        ? profile && <GeneralEditBlock profile={profile} saveProfileGeneral={saveProfileGeneral} cancelEditing={() => setIsEditeGeneral(false)}/>
+                        : <GeneralBlock profile={profile} isLoading={isLoading} isSuccess={isSuccess} startEditMode={() => setIsEditeGeneral(true)}/>
+                    }
                 </Grid.Col>
                 <Grid.Col span={{ base: 12, md: 4 }}>
                     <div className="block">
                         <ActionIcon variant="transparent" size="xl" aria-label="Settings" pos="absolute" top="24px" right="24px">
                             <IconPencil color="black" size="2rem"  stroke={1} />
                         </ActionIcon>
-                        <Title order={3} ta="left">Интересы</Title>
-                        <Text c="black" size="lg" ta="left" >
-                            Плавание
-                        </Text>
-                        <Text c="black" size="lg" ta="left" >
-                            Хоббихорсинг
-                        </Text>
-                    </div>
-                </Grid.Col>
-                <Grid.Col span={{ base: 12 }}>
-                    <div className="block">
-                        <ActionIcon variant="transparent" size="xl" aria-label="Settings" pos="absolute" top="24px" right="24px">
-                            <IconPencil color="black" size="2rem"  stroke={1} />
-                        </ActionIcon>
-                        <Title order={3} ta="left">Профессиональные навыки</Title>
-                        <Group mt={"sm"}>
+                        <Title order={3} ta="left">Ссылки</Title>
+                        <Divider my={"md"}/>
+                        <Stack gap="xs">
                             {
-                                ["Pytorch", "NLP", "Llama"].map(name => (
-                                    <Badge
-                                        variant="outline"
-                                        color="gray"
-                                    >
-                                        {name}
-                                    </Badge>
-                                ))
+                                isLoading
+                                    ? <Skeleton height={30} width={"100%"}/>
+                                    : isSuccess && profile && profile.link_ru_resume !== "" && (
+                                    <Anchor w={"100%"} href={profile.link_ru_resume} target="_blank" underline="hover">
+                                        Резюме (RU)
+                                    </Anchor>
+                                )
                             }
-                        </Group>
+                            {
+                                isLoading
+                                    ? <Skeleton height={30} width={"100%"}/>
+                                    : isSuccess && profile && profile.link_eng_resume !== "" && (
+                                    <Anchor w={"100%"} href={profile.link_eng_resume} target="_blank" underline="hover">
+                                        Резюме (ENG)
+                                    </Anchor>
+                                )
+                            }
+                            {
+                                isLoading
+                                    ? <Skeleton height={30} width={"100%"}/>
+                                    : isSuccess && profile && profile.link_tg_personal !== "" && (
+                                    <Anchor w={"100%"} href={profile.link_tg_personal} target="_blank" underline="hover">
+                                        Telegram
+                                    </Anchor>
+                                )
+                            }
+                            {
+                                isLoading
+                                    ? <Skeleton height={30} width={"100%"}/>
+                                    : isSuccess && profile && profile.link_linkedin !== "" && (
+                                    <Anchor w={"100%"} href={profile.link_linkedin} target="_blank" underline="hover">
+                                        LinkedIn
+                                    </Anchor>
+                                )
+                            }
+                        </Stack>
                     </div>
                 </Grid.Col>
+                {/*<Grid.Col span={{ base: 12 }}>*/}
+                {/*    <div className="block">*/}
+                {/*        <ActionIcon variant="transparent" size="xl" aria-label="Settings" pos="absolute" top="24px" right="24px">*/}
+                {/*            <IconPencil color="black" size="2rem"  stroke={1} />*/}
+                {/*        </ActionIcon>*/}
+                {/*        <Title order={3} ta="left">Навыки</Title>*/}
+                {/*        <Group mt={"sm"}>*/}
+                {/*            {*/}
+                {/*                ["Pytorch", "NLP", "Llama"].map(name => (*/}
+                {/*                    <Badge*/}
+                {/*                        variant="outline"*/}
+                {/*                        color="gray"*/}
+                {/*                    >*/}
+                {/*                        {name}*/}
+                {/*                    </Badge>*/}
+                {/*                ))*/}
+                {/*            }*/}
+                {/*        </Group>*/}
+                {/*    </div>*/}
+                {/*</Grid.Col>*/}
             </Grid>
         </TalentProfileLayout>
     )
